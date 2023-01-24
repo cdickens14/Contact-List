2b637512af321d2d7d60ec7590b89e52a65fc17c
Abed <abedj2023@Gmail.com>
2023-01-23 19:48:57 -0600

    gettint contact list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React from "react";
+import dummyContacts from "./index";
+import { useState } from "react";
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
+  contacts.map((contact) => {
+    return contact.name;
+  });
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
       <div id="container">
-        {/* your components here */}
+        {
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+              </tr>
+
+              {contacts.map((contact) => {
+                return <tr></tr>;
+              })}
+              <tr>
+                <td>Person One</td>
+                <td>000-000-0000</td>
+                <td>one@email.com</td>
+              </tr>
+
+              {/* Contact 2 */}
+              <tr>
+                <td>Person Two</td>
+                <td>111-111-1111</td>
+                <td>two@email.com</td>
+              </tr>
+            </tbody>
+          </table>
+        }
       </div>
     </div>
   );
-}
+};
 
-export default Main
+export default Main;
